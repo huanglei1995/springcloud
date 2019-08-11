a69749391bb12ffc26337f4b342de67a73ec1b47
huanglei1995 <1603805837@qq.com>
2019-08-11 23:30:08 +0800

    添加微信接口类型

---
+++ cn-shop-parent/cn-shop-api-entity-api/cn-shop-service-api-weixin-entity/src/main/java/com/cnshop/entity/AppEntity.java
@@ -0,0 +1,16 @@
+package com.cnshop.entity;
+
+import lombok.Data;
+
+/**
+ * @auther: 黄磊
+ * @date:
+ * @description: 微信应用实体类
+ */
+@Data
+public class AppEntity {
+
+    private String appId;
+
+    private String appName;
+}
---
+++ cn-shop-parent/cn-shop-service-api/cn-shop-service-api-weixin/src/main/java/com/cnshop/weixin/service/WeiXinService.java
@@ -0,0 +1,13 @@
+package com.cnshop.weixin.service;
+
+import com.cnshop.entity.AppEntity;
+
+/**
+ * @auther: 黄磊
+ * @date: 2019/8/11 22:42
+ * @description: 微信公共服务接口
+ */
+public interface WeiXinService {
+
+    public AppEntity getApp();
+}
