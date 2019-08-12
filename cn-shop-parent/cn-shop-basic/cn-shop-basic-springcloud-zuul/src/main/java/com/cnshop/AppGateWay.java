6f713fbc15c3a3cd7a56f7c02688e9f0c5e5fcaf
huanglei1995 <1603805837@qq.com>
2019-08-12 22:47:34 +0800

    添加网关服务，并将swagger接口文档统一在网关显示

---
+++ cn-shop-parent/cn-shop-basic/cn-shop-basic-springcloud-zuul/src/main/java/com/cnshop/AppGateWay.java
@@ -0,0 +1,53 @@
+package com.cnshop;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @auther: 黄磊
+ * @date: 2019/8/12 22:13
+ * @description: 微服务网关
+ */
+@SpringBootApplication
+@EnableSwagger2Doc
+@EnableEurekaClient
+@EnableZuulProxy
+public class AppGateWay {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGateWay.class, args);
+    }
+
+    // 添加文档来源
+    @Component
+    @Primary
+    class DocumentationConfig implements SwaggerResourcesProvider {
+        @Override
+        public List<SwaggerResource> get() {
+            List resources = new ArrayList();
+
+            resources.add(swaggerResource("app-cnshop-number", "/app-cnshop-number/v2/api-docs", "1.0.1"));
+            resources.add(swaggerResource("app-cnshop-weixin", "/app-cnshop-weixin/v2/api-docs", "1.0.1"));
+            return resources;
+        }
+
+        private SwaggerResource swaggerResource (String name, String location, String version) {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion(version);
+            return swaggerResource;
+        }
+    }
+}
