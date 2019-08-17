1dc34cf0e6bd6ef848db0df339b25870b2993ebe
huanglei1995 <1603805837@qq.com>
2019-08-17 21:37:24 +0800

    添加apollo同意配置中心，并解决guava依赖冲突的问题

--- cn-shop-parent/cn-shop-service-impl/cn-shop-service-weixin/src/main/java/com/cnshop/weixin/AppWeixin.java
+++ cn-shop-parent/cn-shop-service-impl/cn-shop-service-weixin/src/main/java/com/cnshop/weixin/AppWeixin.java
@@ -1,5 +1,6 @@
 package com.cnshop.weixin;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2Doc
+@EnableApolloConfig
 public class AppWeixin {
 
     public static void main(String[] args) {
